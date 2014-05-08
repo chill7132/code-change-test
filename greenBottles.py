3077acca52054818dae3b6a8ce9292bbfa9cd5f2
Callum_ <chill7132@sohamcollege.org.uk>
2014-05-08 11:57:24 +0100

    Update greenBottles.py
    This is my green bottles code

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,10 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+bottle = 10
+for x in range(10):
+    x_bottle = bottle-1
+    print("{0} green bottles, hanging on the wall".format(bottle))
+    print("{0} green bottles, hanging on the wall".format(bottle))
+    print("And if 1 green bottle should accidentally fall")
+    print("They'll be {0} green bottles hanging on the wall".format(x_bottle))
+    bottle =bottle-1
+
+
